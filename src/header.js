a77da150da83104e6b6b2dd23e8a14966e1bcd52
James Dinh <jamesdinh77@gmail.com>
2023-10-21 14:48:33 -0700

    updated flask

--- src/header.js
+++ src/header.js
@@ -3,41 +3,88 @@ import './header.scss';
 import {attributes} from './data/attributes.js'
 
 function App() {
-  const [accounts, setAccounts] = useState([]);
+  const [formData, setFormData] = useState({
+    "name": '',
+    "year": '',
+    "major": '',
+    "age": '',
+    "gender": '',
+    "religion": '',
+    "dietpref": '',
+  });
 
-  useEffect(() => {
-    fetch("/accounts", {
-      method:"GET", headers: {
-        "Content-Type": "application/json",
-        'Accept': 'application/json'
-      },
-  }).then(
-      res => res.json()
-    ).then(
-      data => {
-        setAccounts(data);
-        console.log(data);
+  // to display accounts data
+  // useEffect(() => {
+  //   fetch("/accounts", {
+  //     method:"GET", headers: {
+  //       "Content-Type": "application/json",
+  //       'Accept': 'application/json'
+  //     },
+  // }).then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       console.log(data);
+  //     }
+  //   )
+  // }, [])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/accounts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 200) {
+        console.log('Data successfully posted to Flask API');
+        // You can reset the form or perform any other action here
+        fetch("/accounts", {
+          method:"GET", headers: {
+            "Content-Type": "application/json",
+            'Accept': 'application/json'
+          },
+        }).then(
+            res => res.json()
+          ).then(
+            data => {
+              console.log(data);
+            }
+          )
+      } else {
+        console.error('Failed to post data to Flask API');
       }
-    )
-  }, [])
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
 
   return (
     <header className='header'>
-      <form action="/accounts" method='post'>
+      <form onSubmit={handleSubmit}>
         <div className='criteria'>
-            <input className='name-input' id="name-input" name="name" type="text" placeholder="Name"></input>
-            <input className='age-input' id="age-input" type="number" name="age" placeholder="Age"></input>
-            <select className='year' id="year-input">
+            <input className='name-input' id="name-input" type="text" name="name" value={formData["name"]} onChange={handleChange} placeholder="Name"></input>
+            <input className='age-input' id="age-input" type="number" name="age" value={formData["age"]} onChange={handleChange} placeholder="Age"></input>
+            <select className='year' id="year-input" name="year" value={formData["year"]} onChange={handleChange}>
               {attributes["years"].map((item, index) => (
                 <option value={item} key={index}>{item}</option>
               ))}
             </select>
-            <select className='major' id="major-input">
+            <label for="major">Major</label>
+            <select className='major' id="major-input" name="major" value={formData["major"]}>
               {attributes["majors"].map((item, index) => (
                   <option value={item} key={index}>{item}</option>
                 ))}
             </select>
-            
             <select className='interests-input' id="intersts-input">
               {attributes["interests"].map((item, index) => (
                   <option value={item} key={index}>{item}</option>
@@ -47,9 +94,9 @@ function App() {
         <input type='submit' className='searchBtn' value='submit'></input>
       </form>
       <h2 className='results-heading'>Results</h2>
-      {accounts.map((account, i) => (
+      {/* {data.map((account, i) => (
         <p key={i}>{account["name"]}</p>
-      ))}
+      ))} */}
       <div className='results-container'></div>
     </header>
   );
